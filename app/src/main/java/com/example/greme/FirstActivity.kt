0de7da8d0afab3a72c5b8bfbf553c9f475e31809
cyj2825 <cyj2825@naver.com>
2022-11-18 17:38:27 +0900

    첫 화면2 구현 완료

---
+++ app/src/main/java/com/example/greme/FirstActivity.kt
@@ -0,0 +1,25 @@
+package com.example.greme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.greme.databinding.ActivityFirstBinding
+
+class FirstActivity : AppCompatActivity() {
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: ActivityFirstBinding? = null
+    // 매번 null 체크를 할 필요없이 편의성을 위해 바인딩 변수 재선언
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 바인딩
+        mBinding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 함
+        mBinding = null
+        super.onDestroy()
+    }
+}
